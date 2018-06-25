b6d9c63761a0e65aecf803354e72caeb6a135d59
hyeon95y <hyeon95y@gmail.com>
2018-06-25 22:57:24 +0200

    conv1d

--- LMTS/LMTS_Tutorial.py
+++ LMTS/LMTS_Tutorial.py
@@ -1,15 +1,15 @@
-# REFERENCE
+# reference
 # https://machinelearningmastery.com/sequence-classification-lstm-recurrent-neural-networks-python-keras/
 
-# CHECK VERSION OF PYTHON FOR ATOM
+# check the version of the atom
 import sys
 print("Version ",sys.version)
 
-# IMPORT MODULES
+# import modules
 import numpy
 from keras.datasets import imdb
 from keras.models import Sequential
-from keras.layers import Dense, LSTM
+from keras.layers import Dense, LSTM, Conv1D, MaxPooling1D
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 
@@ -25,20 +25,24 @@ max_review_length = 5000
 x_train = sequence.pad_sequences(x_train, maxlen=max_review_length)
 x_test = sequence.pad_sequences(x_test, maxlen=max_review_length)
 
-# create the model
+# create the dense network model
 embedding_vector_length = 32
 model = Sequential()
 model.add(Embedding(top_words, embedding_vector_length, input_length=max_review_length))
+model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
+model.add(MaxPooling1D(pool_size=2))
 model.add(LSTM(100))
+#model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
 
-model.fit(x_train, y_train,
+hist = model.fit(x_train, y_train,
           validation_data=(x_test, y_test),
           epochs=3,
-          batch_size=64
+          batch_size=64,
+          verbose = 1
           )
 
 # Final evaluation of the models
@@ -46,8 +50,22 @@ scores = model.evaluate(x_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
 
 # save model
-
 from keras.models import load_model
-filename = 'test.h5'
+filename = 'conv1D.h5'
 model.save(filename)
 model = load_model(filename)
+
+# see the history
+import matplotlib.pyplot as plt
+fig, loss_ax = plt.subplots()
+acc_ax = loss_ax.twinx()
+loss_ax.plot(hist.history['loss'], 'y', label='train loss')
+loss_ax.plot(hist.history['val_loss'], 'r', label='val loss')
+acc_ax.plot(hist.history['acc'], 'b', label='train acc')
+acc_ax.plot(hist.history['val_acc'], 'g', label='val acc')
+loss_ax.set_xlabel('epoch')
+loss_ax.set_ylabel('loss')
+acc_ax.set_ylabel('accuray')
+loss_ax.legend(loc='upper left')
+acc_ax.legend(loc='lower left')
+plt.show()
